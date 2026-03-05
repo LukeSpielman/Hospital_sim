e64e2f95e3b23193d4048acea2d72a88dbf9fa9c
LukeSpielman <lukelobster10@gmail.com>
2026-03-05 13:30:55 -0500

    Patient_Feature

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,21 @@
+import java.util.UUID;
+
 public class Patient {
+    private UUID patientId;
+
+    public Patient() {
+        patientId = UUID.randomUUID();
+    }
+
+    public UUID getPatientId() {
+        return patientId;
+    }
+
+    public String toString() {
+        return "Patient: " + patientId;
+    }
+
+    public static Patient createPatient() {
+        return new Patient();
+    }
 }
