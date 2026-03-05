cd861b906aaab3015a1cb5d3cb9c796b7ef2039d
yeetu <hussainrehan909@gmail.com>
2026-03-05 13:45:39 -0500

    simulation class basis made

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-                Simulation sim = new Simulation();
+        Simulation sim = new Simulation();
+        sim.setup();
+        sim.run();
+        sim.process();
 
             }
 
