176ac3a1619276c0864e37794c6d5763f20d95b9
yeetu <hussainrehan909@gmail.com>
2026-03-12 01:55:12 -0400

    hospital class related stuff

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,3 +1,25 @@
 public class Hospital {
-public Hospital(){};
+    private Patient[] patients;
+    private int count;
+
+    public Hospital(int capacity) {
+        patients = new Patient[capacity];
+        count = 0;
+    }
+
+    public void addPatient(Patient pat) {
+        if (count < patients.length) {
+            patients[count] = pat;
+            count++;
+        } else {
+            System.out.println("Hospital is full.");
+        }
+    }
+
+    public void printPatients() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(patients[i]);
+        }
+    }
+}
 }
---
+++ test/HospitalTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HospitalTest {
+
+
+    @Test
+    void addPatient() {
+    }
+
+    @Test
+    void printPatients() {
+    }
+}
