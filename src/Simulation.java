4ca57f90109bf3a9ccb6ec036052844a1b6e0efd
yeetu <hussainrehan909@gmail.com>
2026-03-14 20:30:17 -0400

    sim class

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,7 +1,55 @@
+import java.util.Random;
 
 public class Simulation {
 
-    public Simulation setup() {}
-    public Simulation run() {}
-    public Simulation process() {}
-}
+        private static Random rand = new Random();
+
+        public static Random getRandom() {
+            return rand;
+        }
+
+
+        private int totalTime = 600;
+        private int currentTime = 0;
+
+
+        private Hospital hospital;
+
+
+        public Simulation() {}
+
+
+        public void setup() {
+            hospital = new Hospital(50);
+            currentTime = 0;
+        }
+
+
+        public void run() {
+            while (currentTime <= totalTime) {
+                processStep();
+                currentTime += 10;
+            }
+
+            process();
+        }
+
+
+        private void processStep() {
+
+
+            if (rand.nextInt(100) < 66) {
+                Patient p = Patient.createPatient();
+                hospital.addPatient(p);
+                System.out.println("Time " + currentTime + ": New patient admitted.");
+            }
+        }
+
+
+        public void process() {
+            System.out.println("Simulation finished.");
+            System.out.println("Total patients admitted: " + hospital.getPatientCount());
+            hospital.printPatients();
+        }
+    }
+
