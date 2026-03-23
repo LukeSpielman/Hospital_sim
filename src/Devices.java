949827a12b0f5e820233c5e53c0d480be23f459f
LukeSpielman <lukelobster10@gmail.com>
2026-03-22 23:04:29 -0400

    device heirarchy

--- src/Devices.java
+++ src/Devices.java
@@ -1,4 +1,56 @@
-abstract class Device {
-    abstract public Observation sample();
-}
+public abstract class Device {
 
+    public abstract Observation sample(Patient patient, int time);
+
+    public static class TemperatureMonitor extends Device {
+
+        public Observation sample(Patient patient, int time) {
+            double temp = 97.0 + (Simulation.getRandom().nextDouble() * 6.0);
+            return new Temperature(patient, time, temp);
+        }
+    }
+
+    public static class Temperature extends Observation {
+        private double degrees;
+
+        public Temperature(Patient patient, int time, double degrees) {
+            setPatient(patient);
+            setTime(time);
+            this.degrees = degrees;
+        }
+
+        public double getDegrees() {
+            return degrees;
+        }
+
+        public String toString() {
+            return "Time " + getTime() + " Patient " + getPatient().getPatientId() + " Temperature: " + degrees;
+        }
+    }
+
+    public static class HeartRateMonitor extends Device {
+
+        public Observation sample(Patient patient, int time) {
+            int bpm = 50 + Simulation.getRandom().nextInt(101);
+            return new HeartRate(patient, time, bpm);
+        }
+    }
+
+    public static class HeartRate extends Observation {
+        private int bpm;
+
+        public HeartRate(Patient patient, int time, int bpm) {
+            setPatient(patient);
+            setTime(time);
+            this.bpm = bpm;
+        }
+
+        public int getBpm() {
+            return bpm;
+        }
+
+        public String toString() {
+            return "Time " + getTime() + " Patient " + getPatient().getPatientId() + " Heart Rate: " + bpm;
+        }
+    }
+}
